7544b4b434709d692b51253ed8c8f4d228a4179a
Paul Leslie <66122258+pleslie182@users.noreply.github.com>
2020-09-01 10:22:35 +0100

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+// Check off specific todos by clicking & uncheck when clicked again
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete to-do from ul
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//Input new to-do
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new li and add to ul
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle(200);
+});
